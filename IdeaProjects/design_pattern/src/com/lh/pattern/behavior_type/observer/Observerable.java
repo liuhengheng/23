807fa2782678f6c7565eace3bb077aa832e829fd
liuhengheng <liuhengheng@cnpc.com.cn>
2019-01-25 17:12:01 +0800

    观察者模式

---
+++ IdeaProjects/design_pattern/src/com/lh/pattern/behavior_type/observer/Observerable.java
@@ -0,0 +1,14 @@
+package com.lh.pattern.behavior_type.observer;
+
+/***
+ * 抽象被观察者接口
+ * 声明了添加、删除、通知观察者方法
+ *
+ */
+public interface Observerable {
+    public void registerObserver(Observer o);
+
+    public void removeObserver(Observer o);
+
+    public void notifyObserver();
+}
